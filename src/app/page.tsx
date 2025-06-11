0bf8eba3c271061f119e80e4c3935b25c361ac21
EricDCamargo <ericdcamargo@gmail.com>
2025-06-10 23:29:37 -0300

    refactor: home dosent redirect user to adm page if authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import { getUserServer } from '@/services/retriveSSRData/retriveUserData'
 import Events from './(fatec)/(events)/page'
 import FatecLayout from './(fatec)/layout'
-import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const user = await getUserServer()
-
-  if (user) {
-    redirect('/administration')
-  }
   return (
     <FatecLayout>
       <Events />
