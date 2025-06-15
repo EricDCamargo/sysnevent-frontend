7062075172fa15156df08618c8c74f768dbd23b1
EricDCamargo <ericdcamargo@gmail.com>
2025-06-15 03:34:50 -0300

    feat: Enhance DetailEventsPage to display event details and related events

--- src/app/(fatec)/(events)/detailEvents/[event_id]/page.tsx
+++ src/app/(fatec)/(events)/detailEvents/[event_id]/page.tsx
@@ -1,4 +1,7 @@
-import { handleDetailEvent } from '@/services/retriveSSRData/retiveEventData'
+import {
+  getEvents,
+  handleDetailEvent
+} from '@/services/retriveSSRData/retiveEventData'
 import DetailEventsPage from '../detailEventsPage'
 
 interface DetailEventsProps {
@@ -10,6 +13,9 @@ interface DetailEventsProps {
 export default async function DetailEvent({ params }: DetailEventsProps) {
   const { event_id } = await params
   const event = await handleDetailEvent(event_id)
+  const events = await getEvents()
 
-  return <DetailEventsPage event={event} currentEventId={event_id} />
+  return (
+    <DetailEventsPage event={event} events={events} currentEventId={event_id} />
+  )
 }
