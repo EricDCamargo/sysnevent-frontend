e2fdf028e69b37cb30efa9c0ca694f4ed16542a1
EricDCamargo <ericdcamargo@gmail.com>
2025-06-15 00:36:28 -0300

    refactor: remove ManageEventsPage component and associated styles, update Administration page to use EventsPage

--- src/app/(fatec)/administration/page.tsx
+++ src/app/(fatec)/administration/page.tsx
@@ -1,5 +1,4 @@
 import { getEvents } from '@/services/retriveSSRData/retiveEventData'
-import ManageEventsPage from './events/manageEventsPage'
 import EventsPage from '../_components/eventsPage'
 
 export default async function Administration() {
